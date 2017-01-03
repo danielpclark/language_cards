43386cb795926c921ff4dffbf4def2d30df75562
Daniel P. Clark <6ftdan@gmail.com>
2017-01-03 06:19:35 -0500

    Working game

--- lib/language_cards.rb
+++ lib/language_cards.rb
@@ -24,14 +24,27 @@ module LanguageCards
     end
     
     def start_menu
-      clear
-      puts I18n.t 'StartMenu.Title'
-      opt = CLI.choose do |menu|
-        menu.prompt = I18n.t 'StartMenu.Choose'
-        @CARDS.classes.each do |item|
-          menu.choice(item) 
+      loop do
+        clear
+        puts I18n.t 'StartMenu.Title'
+        opt = CLI.choose do |menu|
+          menu.prompt = I18n.t 'StartMenu.Choose'
+          @CARDS.classes.each do |item|
+            menu.choice(item) 
+          end
+          menu.choices(:exit)
         end
-        menu.default = 0
+        break if opt == :exit
+        collection = @CARDS.select_collection(opt)
+        comp_bitz = collection.rand
+        input = CLI.ask("#{I18n.t('Game.TypeThis')} #{collection.mapped_as}: #{comp_bitz.display}")
+        if collection.correct? input, comp_bitz
+          CLI.say "You have won a cookie!"
+          sleep 2
+        else
+          CLI.say "You have failed this PTY!"
+          sleep 2
+        end
       end
     end
 
@@ -70,6 +83,27 @@ module LanguageCards
       end
     end
 
+    def [](value)
+      raise "Invalid option on empty card collection" unless collection?
+      @cards[value]
+    end
+
+    def rand
+      raise "Invalid option on empty card collection" unless collection?
+      v = @cards.keys.sample
+      @comparator.given(v, @cards[v])
+    end
+
+    def correct? input, comp_bitz
+      raise "Invalid option on empty card collection" unless collection?
+      @comparator.match? input, comp_bitz
+    end
+
+    def mapped_as
+      raise "Invalid option on empty card collection" unless collection?
+      @comparator.mapped_as
+    end
+
     def children
       @hsh.select {|k,v| v.is_a? CardCollection}.values.map do |cc|
         {cc.name => cc}
@@ -77,12 +111,12 @@ module LanguageCards
     end
 
     def select_collection string
-      lang, rest = string.split(JOIN, 2)
+      first, tail = string.split(JOIN, 2)
       if collection?
-        @mapping_choice = rest
+        @comparator = @mappings.select_mapping(first)
         return self
       else
-        children[lang].select_collection rest
+        children[first].select_collection tail
       end
     end
 
@@ -99,7 +133,7 @@ module LanguageCards
     # Replace respective raw ruby object with our objects
     def _build
       if @hsh.has_key? "mapping"
-        @hsh["mapping"] = Mappings.new(@hsh["mapping"]) 
+        @hsh["mapping"] = Mappings.new(@hsh["mapping"], self) 
       else
         @hsh.tap do |h|
           h.select {|k,v| v.is_a? Hash }.each do |k,v|
@@ -110,17 +144,57 @@ module LanguageCards
     end
 
     class Comparator
-      def initialize mapping_key, mapping
+      attr_reader :mapped_as
+      def initialize mapping_key, mapping, collection
         ##
         # @title should be a hash like {"Romaji"=>"Katakana"}
         # @mapping should be an array like [:k, :v]
-        @title, @mapping = mapping[mapping_key]
+        @mapped_as, @mapping = mapping[mapping_key].reduce
+        @collection = collection
       end
 
+      def given key, value
+        CompBitz.new(
+          display: choose(key, value),
+          collection: @collection,
+          value: value,
+          mapping: @mapping.first
+        )
+      end
+
+      def match? input, comp_bitz
+        case comp_bitz.mapping 
+        when :k
+          comp_bitz.value == comp_bitz.collection[input]
+        when :v
+          comp_bitz.value == input
+        end
+      end
+
+      private
+      def choose key, value
+        case @mapping.last
+        when :k
+          key
+        when :v
+          value
+        end
+      end
+
+      class CompBitz
+        attr_reader :display, :collection, :value, :mapping
+        def initialize options
+          @display = options.fetch(:display)
+          @collection = options.fetch(:collection)
+          @value = options.fetch(:value)
+          @mapping = options.fetch(:mapping)
+        end
+      end
     end
 
     class Mappings
-      def initialize mapping
+      def initialize mapping, collection=nil
+        @collection = collection
         @mappings = {}
         mapping.each do |h|
           index = h.delete("index")
@@ -131,10 +205,15 @@ module LanguageCards
           end
           a = I18n.t "LanguageName.#{a}"
           b = I18n.t "LanguageName.#{b}"
+
           @mappings["#{a} => #{b}"] = {
             h => index
           }
         end
+      end
+
+      def select_mapping string
+        Comparator.new string, self, @collection
       end
 
       def classes s=nil
