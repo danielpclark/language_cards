3a3f81af0cf8cd786c54c47b0482a8bfb68d0c05
Daniel P. Clark <6ftdan@gmail.com>
2018-04-04 18:01:31 -0400

    comment was wrong

--- lib/language_cards/grapheme_builder.rb
+++ lib/language_cards/grapheme_builder.rb
@@ -2,7 +2,6 @@ require 'language_cards/models/grapheme'
 
 module LanguageCards
   module GraphemeBuilder
-    # Hash {translation => grapheme}
     def self.call(translation_graphemes = {})
       translation_graphemes.each_with_object([]) do |(key, value), memo|
         memo << Grapheme.new(key, value)
